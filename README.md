3d13999b0616d534e610f8cd4bfe6306305a2578
Magana <JMagana@elementcorp.com>
2025-11-06 15:46:19 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+test1
+test v1.53
---
+++ lambda_function.py
@@ -0,0 +1,16 @@
+import os
+
+def lambda_handler(event, context):
+    file_path = os.path.join(os.path.dirname(__file__), '../archivo.txt')
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            contenido = f.read()
+        return {
+            'statusCode': 200,
+            'body': contenido
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f'Error al leer el archivo  : {str(e)}'
+        }
